9e873279232d4dfe928fd9279eaafe64a7be7caa
LFRezende <lfsrsprofessional@gmail.com>
2024-03-26 17:19:37 -0300

    Add: README.md

--- MultiSig.sol
+++ MultiSig.sol
@@ -19,14 +19,23 @@ contract MultiSigWallet {
         uint256 _initialTxChecks
     ) payable {
         // owners = _owners; - It would be ideal for gas sake, yet to ensure no invalid owners:
+
+        require(
+            _owners.length <= _initialTxChecks,
+            "There are more txConfirmations required than signers!"
+        );
         require(
             _owners.length > 0,
             "No address list has been passed to the constructor"
         );
+
         for (uint256 j = 0; j < _owners.length; j++) {
             address payable adm = _owners[j];
             require(adm != address(0), "Null address is invalid.");
             require(!doubleOwner[adm], "There is a double address input.");
+            doubleOwner[adm] = true; // Registration of the owner
+
+            owners.push(adm);
         }
     }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# Multi-Signature Wallet
+
+This project was made for study-purposes only, as well it has been heavily inspired by Solidity-By-Example.
+
+This is not meant to be used in production.
+
+
