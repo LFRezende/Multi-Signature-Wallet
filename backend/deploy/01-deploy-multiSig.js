55e25eab058fc7a25ecbeb9696fdb4913f157cc6
LFRezende <lfsrsprofessional@gmail.com>
2024-04-04 15:12:05 -0300

    Add: Deploy scripts folder

---
+++ backend/deploy/01-deploy-multiSig.js
@@ -0,0 +1,2 @@
+// First of all, require hardhat and use the packages
+const { network, ethers } = require("hardhat");
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
+      accounts: process.env.PRIVATE_KEY,
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
